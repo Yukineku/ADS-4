0b7b660f936bcac8b150a96d061c52bcf6801f31
Yukineku <115874122+Yukineku@users.noreply.github.com>
2024-03-24 23:54:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,57 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                ++count;
+            }
+        }
+    }
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            ++count;
+            ++left;
+            --right;
+        } else if (sum < value) {
+            ++left;
+        } else {
+            --right;
+        }
+    }
+    return count;
+}
+int binarySearchByTarget(int *arr, int len, int target) {
+    int left = 0;
+    int right = len - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+    for (int i = 0; i < len; ++i) {
+        int secondElement = value - arr[i];
+        int index = binarySearchByTarget(arr, len, secondElement);
+        if (index != -1 && index != i) {
+            ++count;
+        }
+    }
+    return count;
 }
+
